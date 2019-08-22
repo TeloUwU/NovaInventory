d3c273013eb96677a15bf618fb04d235490987eb
Josue Henriquez <Josue Henriquez@JOSUEHQUEZ>
2019-08-22 13:42:29 -0600

    Inicio de proyecto

--- NovaInventory/NovaInventory/Vista/FrmPrimerUso.cs
+++ NovaInventory/NovaInventory/Vista/FrmPrimerUso.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace NovaInventory
+namespace NovaInventory.Vista
 {
-    public partial class Form1 : Form
+    public partial class FrmPrimerUso : Form
     {
-        public Form1()
+        public FrmPrimerUso()
         {
             InitializeComponent();
         }
