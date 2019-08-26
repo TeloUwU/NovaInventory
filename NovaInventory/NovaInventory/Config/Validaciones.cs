1013e696b9c321df0f80e870105ebd9213f5146a
Luis <Luis@DESKTOP-S4D4Q2V>
2019-08-26 08:30:10 -0600

    sus logins

---
+++ NovaInventory/NovaInventory/Config/Validaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaInventory.Config
+{
+    class Validaciones
+    {
+       public static string md5 (string value)
+        {
+            MD5CryptoServiceProvider x = new MD5CryptoServiceProvider();
+            byte[] data = Encoding.ASCII.GetBytes(value);
+            data = x.ComputeHash(data);
+            string rest = "";
+            for (int i = 0; i < data.Length; i++)
+                rest += data[i].ToString("x2").ToLower();
+            return rest;
+
+           
+        }
+    }
+}
--- NovaInventory/NovaInventory/Modelo/control_usuario.cs
+++ NovaInventory/NovaInventory/Modelo/control_usuario.cs
@@ -7,6 +7,7 @@ using NovaInventory.Config;
 using NovaInventory.Controlador;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms;
+using System.Data;
 
 namespace NovaInventory.Modelo
 {
@@ -15,7 +16,7 @@ namespace NovaInventory.Modelo
         public static int registro_usuario(constructor_primer_usuario usu)
         {
             int retorno = 0;
-            string query = "SELECT * FROM Datos_empresa WHERE nombre = ?param1 OR nit = ?param2";
+            string query = "SELECT * FROM tbusuarios WHERE nombre = ?param1 OR nit = ?param2";
             try
             {
                 MySqlCommand cmdselect = new MySqlCommand(query, Conexion.obtenerconexion());
@@ -28,7 +29,7 @@ namespace NovaInventory.Modelo
                 }
                 else
                 {
-                    MySqlCommand comandoAgregar = new MySqlCommand(string.Format("INSERT INTO Datos_empresa(nickname, nombre_usuario, apellido_usuario, contraseña_usuario, telefono, Foto_usuario, Correo, nit, id_estados, id_tipo_usuarios, intentos, empresa)VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}')", usu.usuario, usu.nombre_usuario, usu.apellido_usuario, usu.contraseña_usuario, usu.telefono, usu.Foto_usuario, usu.Correo, usu.nit, usu.id_estados, usu.id_tipo_usuarios, usu.intentos, usu.empresa), Conexion.obtenerconexion());
+                    MySqlCommand comandoAgregar = new MySqlCommand(string.Format("INSERT INTO tbusuarios(nickname, nombre_usuario, apellido_usuario, contraseña_usuario, telefono, Foto_usuario, Correo, nit, fecha_de_nacimiento, id_estados, id_tipo_usuarios, intentos, empresa)VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}')", usu.usuario, usu.nombre_usuario, usu.apellido_usuario, usu.contraseña_usuario, usu.telefono, usu.Foto_usuario, usu.Correo, usu.nit, usu.fecha_de_nacimiento, usu.id_estados, usu.id_tipo_usuarios, usu.intentos, usu.empresa), Conexion.obtenerconexion());
                     retorno = Convert.ToInt32(comandoAgregar.ExecuteNonQuery());
                     if (retorno >= 1)
                     {
@@ -42,6 +43,62 @@ namespace NovaInventory.Modelo
                 MessageBox.Show("No se pudo Guardar el usuario/a porfavor verifique si la base de datos esta activa si es asi comuniquese con el administrador " + ex, "ERROR CRITICO", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return retorno;
             }
+       }
+        public static DataTable cargarUSU()
+        {
+            DataTable datos = new DataTable();
+
+            try
+            {
+                string query = "SELECT id_tipo_usuario, CONCAT(tipo_usuario) AS tipo_usuario FROM Tipo_usuario";
+                MySqlCommand mcdquery = new MySqlCommand(query, Conexion.obtenerconexion());
+                MySqlDataAdapter adaptar = new MySqlDataAdapter(mcdquery);
+                adaptar.Fill(datos);
+                return datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("oops, no se pudieron cargar los datos de la base de datos porfavor comunicarse con el programador" + ex, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return datos;
+            }
         }
+        public static DataTable cargar()
+        {
+            DataTable datos = new DataTable();
+
+            try
+            {
+                string query = "SELECT id_estado_usuario, CONCAT(Estado_usuario) AS Estado_usuario FROM estado_usuario";
+                MySqlCommand mcdquery = new MySqlCommand(query, Conexion.obtenerconexion());
+                MySqlDataAdapter adaptar = new MySqlDataAdapter(mcdquery);
+                adaptar.Fill(datos);
+                return datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("oops, no se pudieron cargar los datos de la base de datos porfavor comunicarse con el programador" + ex, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return datos;
+            }
+        }
+        public static DataTable cargar1()
+        {
+            DataTable datos = new DataTable();
+            try
+            {
+                string query = "SELECT id_datos_empresa, CONCAT(nombre) AS nombre FROM Datos_empresa";
+                MySqlCommand mcdquery = new MySqlCommand(query, Conexion.obtenerconexion());
+                MySqlDataAdapter adaptar = new MySqlDataAdapter(mcdquery);
+                adaptar.Fill(datos);
+                return datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("oops, no se pudieron cargar los datos de la base de datos porfavor comunicarse con el programador" + ex, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return datos;
+            }
+        }
+
     }
+
+
 }
