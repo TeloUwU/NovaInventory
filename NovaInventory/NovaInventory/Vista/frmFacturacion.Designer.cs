41932a57055e70121d05ac26add00c58429fb1c6
Diego García <Diego Garca@LAPTOP-PS152N1Q>
2019-09-06 00:06:06 -0600

    Observación que recuerdo de ahorita de el prof Josue

--- NovaInventory/NovaInventory/Vista/frmFacturacion.Designer.cs
+++ NovaInventory/NovaInventory/Vista/frmFacturacion.Designer.cs
@@ -63,52 +63,58 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 35);
+            this.label1.Location = new System.Drawing.Point(49, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(70, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Id Artículo";
             // 
             // txtIdArticulo
             // 
-            this.txtIdArticulo.Location = new System.Drawing.Point(108, 28);
+            this.txtIdArticulo.Location = new System.Drawing.Point(144, 34);
+            this.txtIdArticulo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIdArticulo.Name = "txtIdArticulo";
             this.txtIdArticulo.ShortcutsEnabled = false;
-            this.txtIdArticulo.Size = new System.Drawing.Size(100, 20);
+            this.txtIdArticulo.Size = new System.Drawing.Size(132, 22);
             this.txtIdArticulo.TabIndex = 1;
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(37, 63);
+            this.lblNombre.Location = new System.Drawing.Point(49, 78);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
             this.lblNombre.TabIndex = 2;
             this.lblNombre.Text = "Nombre";
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(37, 89);
+            this.lblPrecio.Location = new System.Drawing.Point(49, 110);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.Size = new System.Drawing.Size(48, 17);
             this.lblPrecio.TabIndex = 3;
             this.lblPrecio.Text = "Precio";
             // 
             // lblCantidad
             // 
             this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Location = new System.Drawing.Point(37, 113);
+            this.lblCantidad.Location = new System.Drawing.Point(49, 139);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblCantidad.Size = new System.Drawing.Size(64, 17);
             this.lblCantidad.TabIndex = 4;
             this.lblCantidad.Text = "Cantidad";
             // 
             // btnCargarLista
             // 
-            this.btnCargarLista.Location = new System.Drawing.Point(22, 194);
+            this.btnCargarLista.Location = new System.Drawing.Point(29, 239);
+            this.btnCargarLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCargarLista.Name = "btnCargarLista";
-            this.btnCargarLista.Size = new System.Drawing.Size(342, 43);
+            this.btnCargarLista.Size = new System.Drawing.Size(456, 53);
             this.btnCargarLista.TabIndex = 5;
             this.btnCargarLista.Text = "Cargar a Lista";
             this.btnCargarLista.UseVisualStyleBackColor = true;
@@ -126,50 +132,57 @@
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.dateTimePicker1);
             this.groupBox1.Controls.Add(this.txtIdArticulo);
-            this.groupBox1.Location = new System.Drawing.Point(22, 28);
+            this.groupBox1.Location = new System.Drawing.Point(29, 34);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(609, 160);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(812, 197);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(108, 106);
+            this.txtCantidad.Location = new System.Drawing.Point(144, 130);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCantidad.Name = "txtCantidad";
             this.txtCantidad.ShortcutsEnabled = false;
-            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.Size = new System.Drawing.Size(132, 22);
             this.txtCantidad.TabIndex = 4;
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(108, 82);
+            this.txtPrecio.Location = new System.Drawing.Point(144, 101);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPrecio.Name = "txtPrecio";
             this.txtPrecio.ShortcutsEnabled = false;
-            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.Size = new System.Drawing.Size(132, 22);
             this.txtPrecio.TabIndex = 3;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(108, 56);
+            this.txtNombre.Location = new System.Drawing.Point(144, 69);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.ShortcutsEnabled = false;
-            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.Size = new System.Drawing.Size(132, 22);
             this.txtNombre.TabIndex = 2;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(245, 31);
+            this.label5.Location = new System.Drawing.Point(327, 38);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.Size = new System.Drawing.Size(47, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Fecha";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(288, 27);
+            this.dateTimePicker1.Location = new System.Drawing.Point(384, 33);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 0;
             // 
             // label6
@@ -178,10 +191,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Lime;
-            this.label6.Location = new System.Drawing.Point(658, 55);
+            this.label6.Location = new System.Drawing.Point(873, 80);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.Size = new System.Drawing.Size(49, 59);
             this.label6.TabIndex = 7;
             this.label6.Text = "$";
             // 
@@ -190,9 +205,11 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
-            this.textBox1.Location = new System.Drawing.Point(650, 52);
+            this.textBox1.Location = new System.Drawing.Point(867, 64);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.Size = new System.Drawing.Size(236, 93);
             this.textBox1.TabIndex = 10;
             // 
             // label7
@@ -200,9 +217,10 @@
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(692, 35);
+            this.label7.Location = new System.Drawing.Point(923, 43);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.Size = new System.Drawing.Size(93, 17);
             this.label7.TabIndex = 10;
             this.label7.Text = "Total a pagar";
             // 
@@ -212,10 +230,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblCostoAPagar.AutoSize = true;
             this.lblCostoAPagar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblCostoAPagar.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCostoAPagar.ForeColor = System.Drawing.Color.Lime;
-            this.lblCostoAPagar.Location = new System.Drawing.Point(692, 55);
+            this.lblCostoAPagar.Location = new System.Drawing.Point(963, 80);
+            this.lblCostoAPagar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCostoAPagar.Name = "lblCostoAPagar";
-            this.lblCostoAPagar.Size = new System.Drawing.Size(28, 13);
+            this.lblCostoAPagar.Size = new System.Drawing.Size(109, 59);
             this.lblCostoAPagar.TabIndex = 11;
             this.lblCostoAPagar.Text = "0.00";
             // 
@@ -224,9 +244,10 @@
             this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(712, 143);
+            this.label12.Location = new System.Drawing.Point(949, 176);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(46, 13);
+            this.label12.Size = new System.Drawing.Size(58, 17);
             this.label12.TabIndex = 10;
             this.label12.Text = "Efectivo";
             // 
@@ -234,10 +255,11 @@
             // 
             this.txtEfectivo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtEfectivo.Location = new System.Drawing.Point(650, 159);
+            this.txtEfectivo.Location = new System.Drawing.Point(867, 196);
+            this.txtEfectivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEfectivo.Name = "txtEfectivo";
             this.txtEfectivo.ShortcutsEnabled = false;
-            this.txtEfectivo.Size = new System.Drawing.Size(178, 20);
+            this.txtEfectivo.Size = new System.Drawing.Size(236, 22);
             this.txtEfectivo.TabIndex = 10;
             // 
             // dgvLista
@@ -253,9 +275,10 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dgvLista.Location = new System.Drawing.Point(22, 244);
+            this.dgvLista.Location = new System.Drawing.Point(29, 300);
+            this.dgvLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvLista.Name = "dgvLista";
-            this.dgvLista.Size = new System.Drawing.Size(823, 253);
+            this.dgvLista.Size = new System.Drawing.Size(1097, 311);
             this.dgvLista.TabIndex = 16;
             // 
             // Column1
@@ -287,9 +310,10 @@
             // 
             this.btnVender.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnVender.Location = new System.Drawing.Point(650, 195);
+            this.btnVender.Location = new System.Drawing.Point(867, 240);
+            this.btnVender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVender.Name = "btnVender";
-            this.btnVender.Size = new System.Drawing.Size(178, 43);
+            this.btnVender.Size = new System.Drawing.Size(237, 53);
             this.btnVender.TabIndex = 6;
             this.btnVender.Text = "Vender";
             this.btnVender.UseVisualStyleBackColor = true;
@@ -297,11 +321,12 @@
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnCerrar});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(866, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1155, 27);
             this.toolStrip1.TabIndex = 18;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -312,15 +337,15 @@
             this.btnCerrar.Image = global::NovaInventory.Properties.Resources.cancel_button;
             this.btnCerrar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(23, 22);
+            this.btnCerrar.Size = new System.Drawing.Size(24, 24);
             this.btnCerrar.Text = "toolStripButton1";
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // frmFacturacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(866, 520);
+            this.ClientSize = new System.Drawing.Size(1155, 640);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.btnVender);
             this.Controls.Add(this.dgvLista);
@@ -333,6 +358,7 @@
             this.Controls.Add(this.btnCargarLista);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmFacturacion";
             this.Text = "frmFacturacion";
             this.Load += new System.EventHandler(this.frmFacturacion_Load);
