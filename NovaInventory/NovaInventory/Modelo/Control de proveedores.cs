9db81575944d45a1ad737a120b16dc344322d789
pc <pc@DESKTOP-38EKH38>
2019-09-08 22:56:24 -0500

    crud de proveedores

--- NovaInventory/NovaInventory/Modelo/Control de proveedores.cs
+++ NovaInventory/NovaInventory/Modelo/Control de proveedores.cs
@@ -20,7 +20,7 @@ namespace NovaInventory.Modelo
             int retorno = 0;
             try
             {
-                MySqlCommand cmdagregar = new MySqlCommand(string.Format("INSERT INTO tbproveedor(nombre, direccion, telefono, nit, rubro, id_estados) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}')", add.nombre, add.direccion, add.telefono, add.nit, add.rubro, add.id_estados),Conexion.obtenerconexion());
+                MySqlCommand cmdagregar = new MySqlCommand(string.Format("INSERT INTO tbproveedor(nombre, direccion, telefono, nit, rubro, id_estados) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}')", add.nombre, add.direccion, add.telefono, add.nit, add.rubro, add.id_estados), Conexion.obtenerconexion());
                 retorno = Convert.ToInt16(cmdagregar.ExecuteNonQuery());
                 if (retorno > 0)
                 {
@@ -37,101 +37,102 @@ namespace NovaInventory.Modelo
         }
 
 
-    //    public static DataTable cargar()
-    //    {
-    //        DataTable datos = new DataTable();
+        //    public static DataTable cargar()
+        //    {
+        //        DataTable datos = new DataTable();
 
-    //        try
-    //        {
-    //            string query = "SELECT id_estado, CONCAT(Estado) AS Estado FROM estado";
-    //            MySqlCommand mcdquery = new MySqlCommand(query, Conexion.obtenerconexion());
-    //            MySqlDataAdapter adaptar = new MySqlDataAdapter(mcdquery);
-    //            adaptar.Fill(datos);
-    //            return datos;
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            MessageBox.Show("oops, no se pudieron cargar los datos de la base de datos porfavor comunicarse con el programador" + ex, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-    //            return datos;
-    //        }
-    //    }
-    //}
+        //        try
+        //        {
+        //            string query = "SELECT id_estado, CONCAT(Estado) AS Estado FROM estado";
+        //            MySqlCommand mcdquery = new MySqlCommand(query, Conexion.obtenerconexion());
+        //            MySqlDataAdapter adaptar = new MySqlDataAdapter(mcdquery);
+        //            adaptar.Fill(datos);
+        //            return datos;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            MessageBox.Show("oops, no se pudieron cargar los datos de la base de datos porfavor comunicarse con el programador" + ex, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //            return datos;
+        //        }
+        //    }
+        //}
 
-    public static DataTable Mostrar_proveedor()
-    {
-        DataTable data;
-        try
+        public static DataTable Mostrar_proveedor()
         {
-            string query = "SELECT * FROM ";
-            MySqlCommand cmdselect = new MySqlCommand(string.Format(query), Conexion.obtenerconexion());
-            MySqlDataAdapter adapter = new MySqlDataAdapter(cmdselect);
-            data = new DataTable();
-            adapter.Fill(data);
-            return data;
-
-        }
-         
-        catch (Exception)
-        {
-
-            MessageBox.Show("A ocurrido un error en la obtencion de datos,consulte con su administrador.", "Error critico de conexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return data = new DataTable();
-        }
-        finally
-        {
-            Conexion.obtenerconexion().Close();
-        }
-        
-    }
-    public static bool Actualizar_proveedor(Constructor_proveedor upd)
-    {
-        bool retorno = false;
-        try
-        {
-            MySqlCommand cmdupd = new MySqlCommand(string.Format("UPDATE  SET nombre = '{0}', WHERE id_proveedor = '{4}'", upd.nombre, upd.direccion, upd.telefono, upd.nit, upd.rubro, upd.id_proveedor), Conexion.obtenerconexion());
-            retorno = Convert.ToBoolean(cmdupd.ExecuteNonQuery());
-            if (retorno == true)
+            DataTable data;
+            try
             {
-                MessageBox.Show("Datos actualizados correctamente.", "Proceso completado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                string query = "SELECT * FROM ";
+                MySqlCommand cmdselect = new MySqlCommand(string.Format(query), Conexion.obtenerconexion());
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmdselect);
+                data = new DataTable();
+                adapter.Fill(data);
+                return data;
+
             }
-            else
+
+            catch (Exception)
             {
-                MessageBox.Show("Los Datos no pudieron ser actualizados.", "Proceso no completado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                MessageBox.Show("A ocurrido un error en la obtencion de datos,consulte con su administrador.", "Error critico de conexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return data = new DataTable();
             }
-            return retorno;
-        }
-        catch (Exception e)
-        {
-            MessageBox.Show("Oops! ha ocurrido un problema, revise su internet o los servicios del servidor, Consulte al Administrador " + e, "Error critico", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return retorno;
-        }
-
-
-    }
-    public static bool Eliminarproveedor(int id)
-    {
-        bool retorno = false;
-        try
-        {
-            MySqlCommand cmddel = new MySqlCommand(string.Format("DELETE FROM tb WHERE id_proveedor = '{0}'", id), Conexion.obtenerconexion());
-            retorno = Convert.ToBoolean(cmddel.ExecuteNonQuery());
-            if (retorno == true)
+            finally
             {
-                MessageBox.Show("El registro se a eliminado correctamente", "Proceso completado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Conexion.obtenerconexion().Close();
             }
-            else
-            {
-                MessageBox.Show("El registro no se eliminado correctamente", "Proceso no completado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            return retorno;
+
         }
-        catch (Exception e)
+        public static bool Actualizar_proveedor(Constructor_proveedor upd)
         {
+            bool retorno = false;
+            try
+            {
+                MySqlCommand cmdupd = new MySqlCommand(string.Format("UPDATE  SET nombre = '{0}', WHERE id_proveedor = '{4}'", upd.nombre, upd.direccion, upd.telefono, upd.nit, upd.rubro, upd.id_proveedor), Conexion.obtenerconexion());
+                retorno = Convert.ToBoolean(cmdupd.ExecuteNonQuery());
+                if (retorno == true)
+                {
+                    MessageBox.Show("Datos actualizados correctamente.", "Proceso completado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Los Datos no pudieron ser actualizados.", "Proceso no completado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                return retorno;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Oops! ha ocurrido un problema, revise su internet o los servicios del servidor, Consulte al Administrador " + e, "Error critico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return retorno;
+            }
 
-            MessageBox.Show("Oops! ha ocurrido un problema, revise su internet o los servicios del servidor, Consulte al Administrador" + e, "Error critico", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return retorno;
+
         }
+        public static bool Eliminarproveedor(int id)
+        {
+            bool retorno = false;
+            try
+            {
+                MySqlCommand cmddel = new MySqlCommand(string.Format("DELETE FROM tb WHERE id_proveedor = '{0}'", id), Conexion.obtenerconexion());
+                retorno = Convert.ToBoolean(cmddel.ExecuteNonQuery());
+                if (retorno == true)
+                {
+                    MessageBox.Show("El registro se a eliminado correctamente", "Proceso completado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("El registro no se eliminado correctamente", "Proceso no completado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                return retorno;
+            }
+            catch (Exception e)
+            {
 
+                MessageBox.Show("Oops! ha ocurrido un problema, revise su internet o los servicios del servidor, Consulte al Administrador" + e, "Error critico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return retorno;
+            }
+
+        }
     }
 }
 
