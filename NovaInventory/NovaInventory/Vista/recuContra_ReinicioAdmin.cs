af9f709a98574d2438f475b0147806e8ee8322e0
Turcios Barquero Andrea María 1 <20190336@ricaldone.edu.sv>
2019-09-17 01:42:32 -0600

    hyh

---
+++ NovaInventory/NovaInventory/Vista/recuContra_ReinicioAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NovaInventory.Vista
+{
+    public partial class recuContra_ReinicioAdmin : Form
+    {
+        public recuContra_ReinicioAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
