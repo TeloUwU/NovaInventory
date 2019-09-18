3b4fb46e3055bd2900259908c8e1fc256a97920a
Turcios Barquero Andrea María 1 <20190336@ricaldone.edu.sv>
2019-09-18 13:19:29 -0600

    jsjs

--- NovaInventory/NovaInventory/Reportes/reporte_usu.Designer.cs
+++ NovaInventory/NovaInventory/Reportes/reporte_usu.Designer.cs
@@ -30,13 +30,26 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.DataSet4 = new NovaInventory.Reportes.DataSet4();
             this.DataTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataSet4 = new NovaInventory.Reportes.DataSet4();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.DataTableTableAdapter = new NovaInventory.Reportes.DataSet4TableAdapters.DataTableTableAdapter();
-            ((System.ComponentModel.ISupportInitialize)(this.DataSet4)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.DataTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // DataTableBindingSource
+            // 
+            this.DataTableBindingSource.DataMember = "DataTable";
+            this.DataTableBindingSource.DataSource = this.DataSet4;
+            // 
+            // DataSet4
+            // 
+            this.DataSet4.DataSetName = "DataSet4";
+            this.DataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // reportViewer1
             // 
@@ -51,32 +64,46 @@
             this.reportViewer1.TabIndex = 0;
             this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
             // 
-            // DataSet4
-            // 
-            this.DataSet4.DataSetName = "DataSet4";
-            this.DataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // DataTableBindingSource
-            // 
-            this.DataTableBindingSource.DataMember = "DataTable";
-            this.DataTableBindingSource.DataSource = this.DataSet4;
-            // 
             // DataTableTableAdapter
             // 
             this.DataTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(360, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Reporte de Usuarios";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NovaInventory.Properties.Resources.portada;
+            this.pictureBox1.Location = new System.Drawing.Point(753, -32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // reporte_usu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1047, 476);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.reportViewer1);
             this.Name = "reporte_usu";
             this.Text = "reporte_usu";
             this.Load += new System.EventHandler(this.reporte_usu_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.DataSet4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DataTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,5 +113,7 @@
         private System.Windows.Forms.BindingSource DataTableBindingSource;
         private DataSet4 DataSet4;
         private DataSet4TableAdapters.DataTableTableAdapter DataTableTableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
