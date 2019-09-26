3306be216a6d72ad5df9de25512c2c9af9f4c9c6
Turcios Barquero Andrea María 1 <20190336@ricaldone.edu.sv>
2019-09-26 00:15:09 -0600

    jyt

--- NovaInventory/NovaInventory/Vista/frmFacturacion.cs
+++ NovaInventory/NovaInventory/Vista/frmFacturacion.cs
@@ -53,18 +53,18 @@ namespace NovaInventory.Vista
             
             
         }
-        string jj = constructorFacturacion.;
+        //string jj = constructorFacturacion.;
         CONSTRUCTORDEFACTURA actu = new CONSTRUCTORDEFACTURA();
         void agregar()
         {
             CONSTRUCTORDEFACTURA agrega = new CONSTRUCTORDEFACTURA();
             agrega.Cantidad = txtCantidad.Text;
             agrega.CostoDetalle = txtPrecio.Text;
-            agrega. = txtDirección.Text;
+            //agrega. = txtDirección.Text;
             agrega.fecha_alta = dateTimePicker1.Text;
-            agrega.creado_por = jj;
-            agrega.id_estado = Convert.ToInt16(cbEstado_Bodega.SelectedValue);
-            int retorno = .agregar_usu(agrega);
+           // agrega.creado_por = jj;
+            //agrega.id_estado = Convert.ToInt16(cbEstado_Bodega.SelectedValue);
+            //int retorno = txtIdArticulo(agrega);
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
