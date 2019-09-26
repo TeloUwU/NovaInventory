fe21adaa04f61487435b542fe3852fb66a6055b6
Turcios Barquero Andrea María 1 <20190336@ricaldone.edu.sv>
2019-09-26 01:08:54 -0600

    yjtj

--- NovaInventory/NovaInventory/Vista/frmGenerarQR.Designer.cs
+++ NovaInventory/NovaInventory/Vista/frmGenerarQR.Designer.cs
@@ -36,6 +36,9 @@
             this.txtnombre = new System.Windows.Forms.TextBox();
             this.btnabrir = new System.Windows.Forms.Button();
             this.btnguardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbmostrararchivo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbgenerar)).BeginInit();
@@ -64,7 +67,7 @@
             // 
             // pbmostrararchivo
             // 
-            this.pbmostrararchivo.Location = new System.Drawing.Point(523, 92);
+            this.pbmostrararchivo.Location = new System.Drawing.Point(465, 130);
             this.pbmostrararchivo.Name = "pbmostrararchivo";
             this.pbmostrararchivo.Size = new System.Drawing.Size(183, 180);
             this.pbmostrararchivo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -73,7 +76,7 @@
             // 
             // pbgenerar
             // 
-            this.pbgenerar.Location = new System.Drawing.Point(31, 92);
+            this.pbgenerar.Location = new System.Drawing.Point(103, 130);
             this.pbgenerar.Name = "pbgenerar";
             this.pbgenerar.Size = new System.Drawing.Size(183, 180);
             this.pbgenerar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -82,14 +85,14 @@
             // 
             // txtmostrar
             // 
-            this.txtmostrar.Location = new System.Drawing.Point(523, 292);
+            this.txtmostrar.Location = new System.Drawing.Point(465, 330);
             this.txtmostrar.Name = "txtmostrar";
             this.txtmostrar.Size = new System.Drawing.Size(183, 20);
             this.txtmostrar.TabIndex = 3;
             // 
             // txtnombre
             // 
-            this.txtnombre.Location = new System.Drawing.Point(31, 292);
+            this.txtnombre.Location = new System.Drawing.Point(103, 330);
             this.txtnombre.Name = "txtnombre";
             this.txtnombre.Size = new System.Drawing.Size(183, 20);
             this.txtnombre.TabIndex = 4;
@@ -97,29 +100,62 @@
             // 
             // btnabrir
             // 
-            this.btnabrir.Location = new System.Drawing.Point(523, 63);
+            this.btnabrir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnabrir.Location = new System.Drawing.Point(465, 368);
             this.btnabrir.Name = "btnabrir";
-            this.btnabrir.Size = new System.Drawing.Size(75, 23);
+            this.btnabrir.Size = new System.Drawing.Size(183, 42);
             this.btnabrir.TabIndex = 5;
             this.btnabrir.Text = "Abrir";
-            this.btnabrir.UseVisualStyleBackColor = true;
+            this.btnabrir.UseVisualStyleBackColor = false;
             this.btnabrir.Click += new System.EventHandler(this.btnabrir_Click_1);
             // 
             // btnguardar
             // 
-            this.btnguardar.Location = new System.Drawing.Point(31, 330);
+            this.btnguardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnguardar.Location = new System.Drawing.Point(103, 368);
             this.btnguardar.Name = "btnguardar";
-            this.btnguardar.Size = new System.Drawing.Size(75, 23);
+            this.btnguardar.Size = new System.Drawing.Size(183, 42);
             this.btnguardar.TabIndex = 6;
             this.btnguardar.Text = "Guardar";
-            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.UseVisualStyleBackColor = false;
             this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Generador de Códigos QR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Generar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Abrir código QR creado previamente";
             // 
             // frmGenerarQR
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 453);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnguardar);
             this.Controls.Add(this.btnabrir);
             this.Controls.Add(this.txtnombre);
@@ -150,5 +186,8 @@
         private System.Windows.Forms.TextBox txtnombre;
         private System.Windows.Forms.Button btnabrir;
         private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
