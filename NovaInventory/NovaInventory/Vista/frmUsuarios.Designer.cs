e77602965982fb1a86fb7cdf7e5a60963b6701ae
Turcios Barquero Andrea María 1 <20190336@ricaldone.edu.sv>
2019-09-26 01:51:48 -0600

    hgfhf

--- NovaInventory/NovaInventory/Vista/frmUsuarios.Designer.cs
+++ NovaInventory/NovaInventory/Vista/frmUsuarios.Designer.cs
@@ -438,7 +438,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(96, 41);
             this.button5.TabIndex = 32;
-            this.button5.Text = "prueba";
+            this.button5.Text = "Generar Reporte";
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
